29038bb5fe6d1069b6bc10a4c39dd6b2bbd52709
Ryan Hennessy <ryanphennessy@icloud.com>
2018-02-17 23:00:59 -0500

    Add search page functionality

---
+++ pulse/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+from twitter import search
+
+
+app = Flask(__name__)
+app.config['DEBUG'] = True # Remove this
+
+
+@app.route('/', methods=['POST', 'GET'])
+def main_page():
+	if request.method == 'POST':
+		hashtag = '#' + request.form['user_search']
+		tweets = search(hashtag)
+		tweets_text = []
+		for tweet in tweets:
+			tweets_text.append(tweet['text'])
+		return render_template('results.html', tweets=tweets_text)
+	else:
+		return render_template('search.html')
+
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0')
